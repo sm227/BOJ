df4f08a6a73e85db25e17e44a8c54d5e330883e1
Seungmin Ham <hamsm3184@naver.com>
2023-10-26 19:27:13 +0900

    Fix

--- src/_1546.java
+++ src/_1546.java
@@ -1,2 +1,41 @@
-package PACKAGE_NAME;public class _1546 {
+import java.awt.datatransfer.FlavorEvent;
+import java.util.Collection;
+import java.util.Scanner;
+import java.util.ArrayList;
+import java.util.*;
+
+public class _1546 {
+    public static void main(String[] args) {
+        int subject ;
+        ArrayList<Integer> score = new ArrayList<Integer>();
+
+        Scanner sc = new Scanner(System.in);
+        subject = sc.nextInt();
+
+        for(int i =0; i<subject; i++) {
+            score.add(sc.nextInt());
+        }
+
+        float maxScore = Collections.max(score);
+
+
+        ArrayList<Float> fake = new ArrayList<Float>();
+
+
+        for (int i =0; i < subject; i++) {
+            fake.add(score.get(i) / maxScore * 100);
+        }
+
+
+        float result = 0;
+        float sum = 0;
+        for (int i=0; i< subject; i++) {
+
+            sum = sum + fake.get(i);
+
+            result = sum / subject;
+        }
+        System.out.println(result);
+
+    }
 }
