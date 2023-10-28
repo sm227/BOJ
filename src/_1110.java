96723462335436639a90da44b9f9677f6947289b
Seungmin Ham <hamsm3184@naver.com>
2023-10-28 16:11:07 +0900

    Fix

--- src/_1110.java
+++ src/_1110.java
@@ -10,38 +10,42 @@ public class _1110 {
 
 		int newNum = 0;
 		int count = 0;
-		
+
 		newNum = num;
 		while (true) {
 			ArrayList<Integer> firstNum = new ArrayList<>();
 			ArrayList<Integer> secondNum = new ArrayList<>();
-			
-			
-			while (newNum > 0) {
-				firstNum.add(newNum % 10);
-				newNum = newNum / 10;
+
+			if (newNum == 0) {
+				count += 1;
+				break;
 			}
 
-			int sum = 0;
-			for (int i = 0; i < firstNum.size(); i++) {
+			else {
+				while (newNum > 0) {
+					firstNum.add(newNum % 10);
+					newNum = newNum / 10;
+				}
 
-				sum = sum + firstNum.get(i);
+				int sum = 0;
+				for (int i = 0; i < firstNum.size(); i++) {
+
+					sum = sum + firstNum.get(i);
+				}
+
+				while (sum > 0) {
+					secondNum.add(sum % 10);
+					sum = sum / 10;
+				}
+
+				String tempNum = String.valueOf(firstNum.get(0)) + String.valueOf(secondNum.get(0));
+
+				newNum = Integer.parseInt(tempNum);
+				count += 1;
+				if (num == newNum)
+					break;
 			}
 
-			while (sum > 0) {
-				secondNum.add(sum % 10);
-				sum = sum / 10;
-			}
-
-			
-
-			String tempNum = String.valueOf(firstNum.get(0)) + String.valueOf(secondNum.get(0));
-
-			
-
-			newNum = Integer.parseInt(tempNum);
-			count += 1;
-			if (num == newNum) break;  
 		}
 		System.out.println(count);
 	}
