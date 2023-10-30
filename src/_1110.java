fff62b07f852d2411fd1ac6fb03176d5867d62c3
Seungmin Ham <hamsm3184@naver.com>
2023-10-30 20:11:28 +0900

    1110번 문제풀이

--- src/_1110.java
+++ src/_1110.java
@@ -1,7 +1,7 @@
 import java.util.Scanner;
 import java.util.ArrayList;
 
-public class _1110 {
+public class _1110 { 
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
